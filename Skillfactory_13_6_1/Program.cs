2c1aad4456dbd63cb08cc817b75d3f30c56bb12d
Serj112 <raskolnikov.112@mail.ru>
2023-01-15 19:45:29 +0300

    Add project files.

---
+++ Skillfactory_13_6_1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        var list = new List<string>();
+        LinkedList<string> l = new LinkedList<string>();
+        string text = File.ReadAllText(@"C:\Users\Toshka\Desktop\Text1.txt");
+
+        //Засекаем обычный список
+        var watchOne = Stopwatch.StartNew();
+        list.Add(text);
+        var a = watchOne.Elapsed.TotalMilliseconds;
+        Console.WriteLine($"Вставка в обычный список: {a}  мс");
+
+
+        //Засекаем связанный список
+        var watchTwo = Stopwatch.StartNew();
+        list.Add(text);
+        var b = watchTwo.Elapsed.TotalMilliseconds;
+        Console.WriteLine($"Вставка в связанный список: {b}  мс");
+
+        //Вычисляем разницу
+        var raznitsa = (a - b);
+        Console.WriteLine($"Вставка текста в связанный список быстрее на {raznitsa} мс");
+    }
+}
